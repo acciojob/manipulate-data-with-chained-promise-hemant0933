4a960bf4867baa5c66e7b1c54858284e6bd04848
Hemant kumar <116268777+hemant0933@users.noreply.github.com>
2023-03-17 13:14:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-//your JS code here. If required.
-
-
-function MyPromise(arr){
-	return new Promise((resolve)=>{
-		setTimeout(()=>{
-			const filteredArr = arr.filter(item => item%2 === 0);
-
-			const manipulatedArr = filteredArr.map(num => num%2 === 0 ? num*2 : num);
-			resolve(filteredArr , manipulatedArr);
-		},3000)
-	})
+function manipulateData(arr) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(arr);
+    }, 3000);
+  })
+  .then((arr) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const filteredArr = arr.filter(num => num % 2 === 0);
+        resolve(filteredArr);
+      }, 1000);
+    });
+  })
+  .then((arr) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const multipliedArr = arr.map(num => num * 2);
+        resolve(multipliedArr);
+      }, 2000);
+    });
+  })
+  .then((arr) => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerText = arr.join(", ");
+  });
 }
 
-MyPromise([1,2,3,4]).then(filteredArr =>{
-	 setTimeout(() => {
-      document.getElementById("output").innerHTML = filteredArr.toString();
-    }, 1000);
-	    return filteredArr;
-
-}).then(manipulatedArr=>{
-	setTimeout(() => {
-		document.getElementById("output").innerHTML = manipulatedArr.toString();
-	},2000)
-	
-})
+manipulateData([1, 2, 3, 4]);
